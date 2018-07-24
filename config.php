a7098204506983296fc6b15ba39a2b0ffd83008a
Karanjot Singh <karanjot21@gmail.com>
2018-07-24 16:47:00 +0530

    imp

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // Configuration file
-$config['App_ID']        =   '643299915817228'; // Your Facebook Application ID
-$config['App_Secret']    =   '3caed52f90eb4a76f5c5aa01e40226ae'; // Your Facebook Application Secret
+$config['App_ID']        =   ''; // Your Facebook Application ID
+$config['App_Secret']    =   ''; // Your Facebook Application Secret
 ?>
